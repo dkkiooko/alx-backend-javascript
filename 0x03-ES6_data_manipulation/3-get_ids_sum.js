fa4833acc567d7a851878df80fca6fa4e0fbf931
Dan Kioko <kithekadankioko@gmail.com>
2024-03-13 03:55:19 +0300

    removed the test for Array type

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -3,11 +3,5 @@
  * @returns sum of student ids
 */
 export default function getStudentIdsSum(students) {
-  if (students instanceof Array) {
-    return students.reduce(
-      (prevStudent, curStudent) => (prevStudent.id !== undefined ? prevStudent.id : prevStudent) + curStudent.id,
-      0,
-    );
-  }
-  return 0;
+  return students.reduce((agg, student) => agg + student.id, 0);
 }
